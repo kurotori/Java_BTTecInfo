5f361207ff2a169f1853d031215b32a9714b0564
Sebastián de los Ángeles <luissebastiananageles@gmail.com>
2025-10-02 16:14:29 -0300

    Agregar método para hashear datos y mejorar la clase Seguridad; incluir archivo .gitignore

--- JavaBase/base-sistema/src/main/java/com/example/herramientas/Seguridad.java
+++ JavaBase/base-sistema/src/main/java/com/example/herramientas/Seguridad.java
@@ -1,5 +1,6 @@
 package com.example.herramientas;
 
+import java.security.MessageDigest;
 import java.util.Random;
 
 public class Seguridad {
@@ -21,9 +22,37 @@ public class Seguridad {
         return resultado;
     }
 
+    public String hashearDato(String dato) {
+        String hash = "";
+        try {
+            MessageDigest md = MessageDigest.getInstance("SHA-256");
+            byte[] datos = dato.getBytes();
+            byte[] hashDatos = md.digest(datos);
+            StringBuilder constructor = new StringBuilder();
+
+            for (byte b : hashDatos) {
+                constructor.append(
+                        String.format("%02x", b));
+            }
+
+            hash = constructor.toString();
+
+        } catch (Exception error) {
+            error.printStackTrace();
+        }
+
+        return hash;
+    }
+
     public static void main(String[] args) {
         Seguridad s = new Seguridad();
-        System.out.println(s.crearSal(20));
+
+        String sal = s.crearSal(20);
+        System.out.println(sal);
+
+        String hash = s.hashearDato(sal);
+        System.out.println(hash);
+
     }
 
 }
